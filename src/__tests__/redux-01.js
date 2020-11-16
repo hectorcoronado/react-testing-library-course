3d67952eea21612eff8e69564d28e851e706bf51
Hector Coronado <hector.coronado@autodesk.com>
2020-11-16 14:45:10 -0800

    test component connected to redux

--- src/__tests__/redux-01.js
+++ src/__tests__/redux-01.js
@@ -1,15 +1,23 @@
 import React from 'react'
+/**
+ * we need to wrap the rendered component in a <Provider />
+ */
 import {Provider} from 'react-redux'
-import {render, fireEvent} from '@testing-library/react'
+import {fireEvent, render} from '@testing-library/react'
 import {Counter} from '../redux-counter'
 import {store} from '../redux-store'
 
 test('can render with redux with defaults', () => {
+  /**
+   * we need `getByText` to grab the 'increment' & 'decrement' btns
+   * we need `getByLabelText` to grab the span with the value
+   */
   const {getByLabelText, getByText} = render(
     <Provider store={store}>
       <Counter />
     </Provider>,
   )
+
   fireEvent.click(getByText('+'))
   expect(getByLabelText(/count/i)).toHaveTextContent('1')
 })
