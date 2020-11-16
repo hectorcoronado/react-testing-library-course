241c17b23a950484faf5ba0f3582c4b2b4d41ca9
Hector Coronado <hector.coronado@autodesk.com>
2020-11-16 14:43:27 -0800

    mock single function in lieu of module

--- src/__tests__/dependency-injection.js
+++ src/__tests__/dependency-injection.js
@@ -3,6 +3,14 @@ import {render, fireEvent, wait} from '@testing-library/react'
 import {GreetingLoader} from '../greeting-loader-02-dependency-injection'
 
 test('loads greetings on click', async () => {
+  /**
+   * we're doing something similar to what we did in `http-jest-mock.js`
+   * where we mocked the entire `api` module; the difference is that instead
+   * of mocking the entire module, we're just mocking a single function
+   * 
+   * it's generally better to stick with `jest.mock`, but certain environments,
+   * such as Storybook, do not support it, so this is a useful alternative.
+   */
   const mockLoadGreeting = jest.fn()
   const testGreeting = 'TEST_GREETING'
   mockLoadGreeting.mockResolvedValueOnce({data: {greeting: testGreeting}})
--- src/greeting-loader-02-dependency-injection.js
+++ src/greeting-loader-02-dependency-injection.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import * as api from './api'
 
+/**
+ * we want to pass in loadGreeting as a prop so that our tests
+ * function as expected, but since we do not want to have to pass
+ * in this prop everywhere we're using our component, we can
+ * set its default value here
+ */
 function GreetingLoader({loadGreeting = api.loadGreeting}) {
   const [greeting, setGreeting] = React.useState('')
   async function loadGreetingForInput(e) {
